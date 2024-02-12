ef8a9948d900cdd2d0a6237acb0e579cff3db222
kingyx333 <kingyx33@gmail.com>
2024-02-12 21:41:56 +0800

    Amended BookingDetail test helper logic

--- app/e2e/helpers.js
+++ app/e2e/helpers.js
@@ -25,8 +25,8 @@ export const testBookingDetail = async () => {
     if (cancelledLabel == 'Booking Cancelled') {
         // Cannot see contact host/user
         await expect(element(by.id("contact-host-or-user"))).not.toBeVisible()
-        // Can see cancel booking (as booking completed)
-        await expect(element(by.text("Booking Cancelled"))).toBeVisible() // Unable to test disabled status of buttons at the moment
+        // Can see cancel booking (as booking cancelled)
+        await expect(element(by.id("cancel-booking"))).toHaveText("Booking Cancelled") // Unable to test disabled status of buttons at the moment
         // Cannot see write review
         await expect(element(by.id("write-review"))).not.toBeVisible()
     } else {
@@ -74,7 +74,7 @@ export const testBookingDetail = async () => {
                 // Can see contact host/user
                 await expect(element(by.id("contact-host-or-user"))).toBeVisible()
                 // Can see cancel booking (as cancel booking)
-                await expect(element(by.text("Cancel Booking"))).toBeVisible() //
+                await expect(element(by.id("cancel-booking"))).toHaveText("Cancel Booking")
                 // Cannot see write review
                 await expect(element(by.id("write-review"))).not.toBeVisible()
 
@@ -86,7 +86,7 @@ export const testBookingDetail = async () => {
                 // Can see contact host/user
                 await expect(element(by.id("contact-host-or-user"))).toBeVisible()
                 // Can see cancel booking (as cancel booking (disabled))
-                await expect(element(by.text("Cancel Booking"))).toBeVisible() // Unable to test disabled status of buttons at the moment
+                await expect(element(by.id("cancel-booking"))).toHaveText("Cancel Booking") // Unable to test disabled status of buttons at the moment
                 // Cannot see write review
                 await expect(element(by.id("write-review"))).not.toBeVisible()
 
@@ -97,7 +97,7 @@ export const testBookingDetail = async () => {
                 // Can see contact host/user
                 await expect(element(by.id("contact-host-or-user"))).toBeVisible()
                 // Can see cancel booking (as booking completed)
-                await expect(element(by.text("Booking Completed"))).toBeVisible() // Unable to test disabled status of buttons at the moment
+                await expect(element(by.id("cancel-booking"))).toHaveText("Booking Completed") // Unable to test disabled status of buttons at the moment
                 // Can see write review
                 await expect(element(by.id("write-review"))).toBeVisible()
 
@@ -109,7 +109,7 @@ export const testBookingDetail = async () => {
                 // Cannot see contact host/user
                 await expect(element(by.id("contact-host-or-user"))).not.toBeVisible()
                 // Can see cancel booking (as booking completed)
-                await expect(element(by.text("Booking Completed"))).toBeVisible() // Unable to test disabled status of buttons at the moment
+                await expect(element(by.id("cancel-booking"))).toHaveText("Booking Completed") // Unable to test disabled status of buttons at the moment
                 // Cannot see write review
                 await expect(element(by.id("write-review"))).not.toBeVisible()
             }
