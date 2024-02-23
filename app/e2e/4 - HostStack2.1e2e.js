e5e35d4280da9b2ad39206a924f4be206ab8fb0e
kingyx333 <kingyx33@gmail.com>
2024-02-23 20:15:57 +0800

    Increased scroll to 250 => 300 for TS4.2

--- app/e2e/4 - HostStack2.1e2e.js
+++ app/e2e/4 - HostStack2.1e2e.js
@@ -28,7 +28,7 @@ describe('Hosting - Manage Space', () => {
   });
 
   it('Navigate to SpaceDetail screen', async () => {
-    await element(by.id('hosting-flatlist')).scroll(250, 'down', NaN, 0.95);
+    await element(by.id('hosting-flatlist')).scroll(300, 'down', NaN, 0.95);
     await expect(element(by.id('0_space'))).toBeVisible();
     await element(by.id('0_space')).tap();
 
