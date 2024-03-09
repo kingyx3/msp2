7a476b80e5f5df6e7207921e6a9e0596fc87b0d1
kingyx333 <kingyx3@hotmail.com>
2024-03-09 20:58:44 +0800

    Deploy v35 to playstore

--- app/app.config.js
+++ app/app.config.js
@@ -50,7 +50,7 @@ module.exports = {
   "userInterfaceStyle": "automatic",
   "android": {
     "package": process.env.ANDROID_ID,
-    "versionCode": 34,
+    "versionCode": 35,
     "adaptiveIcon": {
       "foregroundImage": "./app/assets/adaptive-icon.png",
       "backgroundColor": "#ffffff"
