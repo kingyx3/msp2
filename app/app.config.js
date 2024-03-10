22933ca38478d05d32df802624a6b081f7293618
kingyx333 <kingyx3@hotmail.com>
2024-03-10 15:59:26 +0800

    Deploy v39 with local eas build

--- app/app.config.js
+++ app/app.config.js
@@ -49,7 +49,7 @@ module.exports = {
   "userInterfaceStyle": "automatic",
   "android": {
     "package": process.env.ANDROID_ID,
-    "versionCode": 38,
+    "versionCode": 39,
     "adaptiveIcon": {
       "foregroundImage": "./app/assets/adaptive-icon.png",
       "backgroundColor": "#ffffff"
