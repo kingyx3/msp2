bad65fac0e8e277433952916c560bd80654aa0bd
kingyx3 <kingyx3@hotmail.com>
2024-03-13 19:29:15 +0800

    Update web

---
+++ web/envConfig.js
@@ -0,0 +1,23 @@
+const envVars = REACT_APP_NODE_ENV == "PROD"
+? {
+REACT_APP_TYPE:"DEV",
+REACT_APP_DLINK:"mspdev.page.link",
+REACT_APP_FB_apiKey:"AIzaSyCmMVqDNKg07ftVZwBGAAe8_6nuj0wo5Ho",
+REACT_APP_FB_authDomain:"makeshiftplans-dev.firebaseapp.com",
+REACT_APP_FB_databaseURL:"https://makeshiftplans-dev-default-rtdb.asia-southeast1.firebasedatabase.app",
+REACT_APP_FB_projectId:"makeshiftplans-dev",
+REACT_APP_FB_storageBucket:"makeshiftplans-dev.appspot.com",
+REACT_APP_FB_messagingSenderId:505686398711,
+REACT_APP_FB_appId:"1:505686398711:web:2bdf7092493c5f0a5ae5f2"
+}
+: {
+REACT_APP_TYPE:"DEV",
+REACT_APP_DLINK:"mspdev.page.link",
+REACT_APP_FB_apiKey:"AIzaSyCmMVqDNKg07ftVZwBGAAe8_6nuj0wo5Ho",
+REACT_APP_FB_authDomain:"makeshiftplans-dev.firebaseapp.com",
+REACT_APP_FB_databaseURL:"https://makeshiftplans-dev-default-rtdb.asia-southeast1.firebasedatabase.app",
+REACT_APP_FB_projectId:"makeshiftplans-dev",
+REACT_APP_FB_storageBucket:"makeshiftplans-dev.appspot.com",
+REACT_APP_FB_messagingSenderId:505686398711,
+REACT_APP_FB_appId:"1:505686398711:web:2bdf7092493c5f0a5ae5f2"
+}
