81ab84460f4d1ed397f20f3a755b271ac06f9bf0
kingyx333 <kingyx3@hotmail.com>
2024-05-01 23:47:37 +0800

    Added spacing on left footer

--- web/src/components/NavBarBtm.js
+++ web/src/components/NavBarBtm.js
@@ -8,34 +8,12 @@ const currentYear = new Date().getFullYear();
 const NavBarBtm = () => {
     return (
         <footer style={styles.footer}>
-            <p>&copy; {currentYear} {envVars.REACT_APP_NAME} · <Link to="/policies" style={styles.link}> Policies</Link></p>
+            <p>   &copy; {currentYear} {envVars.REACT_APP_NAME} · <Link to="/policies" style={styles.link}> Policies</Link></p>
         </footer>
     );
 };
 
 const styles = {
-    // nav: {
-    //     background: 'purple',
-    //     position: 'fixed',
-    //     bottom: '0', // Move to the bottom of the screen
-    //     width: '100%',
-    //     zIndex: '999',
-    // },
-    // ul: {
-    //     listStyleType: 'none',
-    //     margin: '0',
-    //     padding: '10px 20px',
-    //     display: 'flex',
-    //     justifyContent: 'space-between', // Adjust for spacing on both ends
-    // },
-    // li: {
-    //     marginLeft: '20px',
-    // },
-    // link: {
-    //     textDecoration: 'none',
-    //     color: 'white',
-    //     fontSize: '16px',
-    // },
     footer: {
         textAlign: 'left',
         position: 'fixed',
