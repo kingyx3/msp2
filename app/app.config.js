900c4c57763c0f6233fa47bed6ef5a12a583554d
kingyx333 <kingyx3@hotmail.com>
2024-05-12 21:06:13 +0800

    Create v47

--- app/app.config.js
+++ app/app.config.js
@@ -7,7 +7,8 @@ module.exports = {
     "android"
   ],
   "plugins": [
-    "@config-plugins/detox", "expo-secure-store"
+    "@config-plugins/detox",
+    "expo-secure-store"
   ],
   "version": process.env.EXPO_PUBLIC_nativeApplicationVersion,
   "orientation": "portrait",
