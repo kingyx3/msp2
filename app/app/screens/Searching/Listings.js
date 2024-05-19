d146514afe0a9d0ca2114a4c95d41b13bc29b8ab
kingyx3 <kingyx3@hotmail.com>
2024-05-19 14:57:23 +0800

    Update Listings.js

--- app/app/screens/Searching/Listings.js
+++ app/app/screens/Searching/Listings.js
@@ -90,7 +90,7 @@ const Listings = (props) => {
       property={selectedSpaceType}
       title={(item.location.suburb ? item.location.suburb : item.location.street) + ', ' + item.location.country}
       subtitle={"$" + item.periodPrice}
-      instantBook={!item.needHostConfirm}
+      caption={item.needHostConfirm ? null : "Insta-Book"}
       rating={item.rating}
       reviews={item.reviews} // number of reviews
       onPress={() => item.third_party ? null : submit(item.id)} //pass marker info?
