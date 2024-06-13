d325df5327989fda7285beace997a63371bfb3ea
kingyx3 <kingyx3@hotmail.com>
2024-06-13 10:12:25 +0800

    Update helpers.js

--- app/e2e/helpers.js
+++ app/e2e/helpers.js
@@ -1,3 +1,12 @@
+const navigateToHomeScreen = async () => {
+  await waitFor(element(by.text('Wallet Balance'))).toBeVisible().withTimeout(30000);
+  await waitFor(element(by.id('search-bar'))).toBeVisible().withTimeout(30000);
+  await expect(element(by.text("Continue with Email"))).not.toBeVisible();
+
+  const walletBalanceLabel = (await element(by.id("wallet-balance")).getAttributes()).label;
+  const match = walletBalanceLabel.match(/(\d+\.\d+)/);
+  global.beginWalletBalance = match ? parseFloat(match[0]) : 0;
+};
 
 export function createSpaceTestSuite() {
     it('Navigate to HostingStep1', async () => {
