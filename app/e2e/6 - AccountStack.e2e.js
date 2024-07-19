e8de4da55fdf5edb84ccc62d07a12b8423a419f4
kingyx333 <kingyx3@hotmail.com>
2024-07-19 23:26:08 +0800

    Switch TS6 (back to TS6 i/o TS1 x 2)

--- app/e2e/6 - AccountStack.e2e.js
+++ app/e2e/6 - AccountStack.e2e.js
@@ -1,39 +1,16 @@
-// AuthStack & HomeStack
-import { navigateToHomeScreen } from "./helpers";
+// AccountStackModal
+import { testBookingDetail, navigateToHomeScreen } from "./helpers";
 
-describe('AuthStack & Home Stack', () => {
+describe('Check AccountStack & ensure navigation works across bookings & spaces', () => {
   beforeAll(async () => {
     await device.launchApp({
       permissions: {
-        location: 'always', // inuse||never||unset
+        // location: 'always', // inuse||never||unset
         photos: 'YES',
         // medialibrary: 'YES',
       }
     });
     await device.disableSynchronization(); // Disable synchronization
-  });
-
-  it('Login screen displays correctly', async () => {
-    await waitFor(element(by.id('email-input'))).toBeVisible().withTimeout(60000);
-    await expect(element(by.id('welcome-text'))).toBeVisible();
-    await expect(element(by.text('Continue with Email'))).toBeVisible();
-  });
-
-  it('Navigate to EmailLinkSent screen', async () => {
-    await element(by.id('email-input')).replaceText('kingyx6@gmail.com'); // kingyx4@gmail.com
-    await element(by.id('welcome-text')).multiTap(3);
-    await element(by.id('submit-email-button')).tap();
-
-    // Wait for navigation to complete & perform the visibility checks
-    await waitFor(element(by.text('Login Link Sent'))).toBeVisible().withTimeout(60000);
-    await waitFor(element(by.text('Continue with Email'))).not.toBeVisible();
-  });
-
-  it('Navigate back to Login screen', async () => {
-    await element(by.id("back-button")).tap();
-
-    // Wait for navigation to complete & perform the visibility checks
-    await waitFor(element(by.id('welcome-text'))).toBeVisible().withTimeout(60000);
   });
 
   it('Navigate to Home screen (Dev Authentication)', async () => {
@@ -42,194 +19,126 @@ describe('AuthStack & Home Stack', () => {
     await element(by.id('submit-email-button')).tap();
 
     // Wait for navigation to complete & perform the visibility checks
-    await navigateToHomeScreen()
-  });
-
-  it('Navigate to TopUp screen', async () => {
-    await element(by.id('top-up-button')).tap();
-
-    // Wait for navigation to complete & perform the visibility checks
-    await waitFor(element(by.text('Top Up Wallet'))).toBeVisible().withTimeout(60000);
-    await expect(element(by.text("Top Up Amount"))).toBeVisible()
-  });
-
-  it('TopUp $100 can be selected & Navigate back to Home screen', async () => {
-    await element(by.id('top-up-button-100')).tap();
-
-    // Wait for action to complete & perform the visibility checks
-    await waitFor(element(by.text('SGD 100'))).toBeVisible().withTimeout(60000);
-    await expect(element(by.text('SGD 100'))).toBeVisible()
-
-    await element(by.id("back-button")).tap();
-
-    // Wait for navigation to complete & perform the visibility checks
     await navigateToHomeScreen();
   });
 
-  it('Navigate to Account Screen & Log Out', async () => { // Log back in
+  it('Navigate to Account screen', async () => {
     await element(by.id('btm-nav-account')).tap();
 
     // Wait for navigation to complete & perform the visibility checks
     await waitFor(element(by.id('account-header-component'))).toBeVisible().withTimeout(60000);
-    await element(by.id('log-out-button')).tap();
-
-    // Wait for navigation to complete & perform the visibility checks
-    await waitFor(element(by.id('email-input'))).toBeVisible().withTimeout(60000);
   });
 
-  it('Navigate to Home screen (Dev Authentication)', async () => { // Log back in
-    await element(by.id('email-input')).replaceText('kingyx3@hotmail.com');
-    await element(by.id('welcome-text')).multiTap(3);
-    await element(by.id('submit-email-button')).tap();
+  it('Change user name', async () => {
+    const randomText = (Math.floor(1000 + Math.random() * 9000)).toString();
+    const newUserName = 'newUserName' + randomText + "   "
+    const oldUserNameLabel = (await element(by.id('username-input')).getAttributes()).label
+
+    await element(by.id('edit-input')).tap();
+    await element(by.id('username-input')).tap();
+    await element(by.id('username-input')).clearText();
+    await element(by.id('username-input')).typeText(newUserName);
+    await element(by.id('save-input')).tap(); // Closes keyboard
+    await element(by.id('save-input')).tap(); // Saves username
+    // await element(by.id('cancel-input')).tap();
+    await waitFor(element(by.id('save-input'))).not.toBeVisible().withTimeout(30000); // Saves username
+    await waitFor(element(by.id('edit-input'))).toBeVisible().withTimeout(30000); // Saves username
+
+    const newUserNameLabel = (await element(by.id('username-input')).getAttributes()).label
+
+    console.log("oldUserNameLabel", "|" + oldUserNameLabel + "|")
+    console.log("newUserNameLabel", "|" + newUserNameLabel + "|")
+    console.log("newUserName", "|" + newUserName + "|")
+
+    await expect(element(by.id('username-input'))).toHaveLabel(newUserName);
+  });
+
+  it('Change user avatar (needs work)', async () => {
+    // const randomText = (Math.floor(1000 + Math.random() * 9000)).toString();
+    // await element(by.id('edit-input')).tap();
+
+
+    // // Wait for navigation to complete & perform the visibility checks
+    // await waitFor(element(by.id('username-input'))).toHaveLabel('new_username' + randomText);
+  });
+
+  it('Navigate to Activity screen (user)', async () => {
+    await element(by.id('Your activity_account')).tap();
 
     // Wait for navigation to complete & perform the visibility checks
-    await navigateToHomeScreen()
+    await waitFor(element(by.id('activity-header-component'))).toBeVisible().withTimeout(60000);
+    await waitFor(element(by.text('Booking activity'))).toBeVisible().withTimeout(60000);
   });
+
+  // it('Attempt user log tap', async () => {
+  //   await element(by.id('user-top-tab')).tap(); // Not always necessary
+  //   await waitFor(element(by.id('0_user_log'))).toBeVisible().withTimeout(60000);
+  //   const userLogLabel = (await element(by.id("0_user_log")).getAttributes()).label
+  //   console.log(userLogLabel + "USER_CONSOLE_LOG_OUTPUT")
+
+  //   if (userLogLabel == 'Top up') {
+  //     // Top up
+  //     null
+  //   } else {
+  //     // Booking (CD)
+  //     await testBookingDetailFromActivity(false)
+  //   }
+  // });
+
+  it('Navigate to Activity screen (host)', async () => {
+    await element(by.id('host-top-tab')).tap();
+
+    // Wait for navigation to complete & perform the visibility checks
+    await waitFor(element(by.id('activity-header-component'))).toBeVisible().withTimeout(60000);
+    await waitFor(element(by.text('Hosting activity'))).toBeVisible().withTimeout(60000);
+  });
+
+  // it('Attempt host log tap (needs work)', async () => {
+  //   await element(by.id('host-top-tab')).tap(); // Not always necessary
+  //   await waitFor(element(by.id('0_host_log'))).toBeVisible().withTimeout(60000);
+  //   const hostLogLabel = (await element(by.id("0_host_log")).getAttributes()).label
+  //   console.log(hostLogLabel + "HOST_CONSOLE_LOG_OUTPUT")
+
+  //   const spaceLogTypeArray = ['Space Created', 'Space Updated', 'Space Disabled', 'Space Enabled', 'Price/Availability Updated']
+
+  //   if (spaceLogTypeArray.includes(hostLogLabel)) {
+  //     // Space (CUD)
+  //     null
+  //   } else {
+  //     // Booking (CD)
+  //     await testBookingDetailFromActivity(true)
+  //   }
+  // });
+
+  it('Navigate back to Account screen', async () => {
+    await element(by.id("back-button-activity")).tap();
+
+    await waitFor(element(by.id('account-header-component'))).toBeVisible().withTimeout(60000);
+  })
 });
 
+// const testBookingDetailFromActivity = async (host) => {
+//   // navigate to BookingDetail screen
+//   await element(by.id('0_' + (host ? "host" : "user") + '_log')).tap();
 
+//   await waitFor(element(by.id("booking-detail-scroll-view"))).toBeVisible(100).withTimeout(60000)
+//   await waitFor(element(by.id("booking-title"))).toBeVisible(100).withTimeout(10000)
+//   await waitFor(element(by.id("booking-price"))).toBeVisible(100).withTimeout(10000)
 
-// // AccountStackModal
-// import { testBookingDetail, navigateToHomeScreen } from "./helpers";
+//   await testBookingDetail()
 
-// describe('Check AccountStack & ensure navigation works across bookings & spaces', () => {
-//   beforeAll(async () => {
-//     await device.launchApp({
-//       permissions: {
-//         // location: 'always', // inuse||never||unset
-//         photos: 'YES',
-//         // medialibrary: 'YES',
-//       }
-//     });
-//     await device.disableSynchronization(); // Disable synchronization
-//   });
+//   // navigate back to Activity screen
+//   await element(by.id("back-button-booking-detail")).tap();
 
-//   it('Navigate to Home screen (Dev Authentication)', async () => {
-//     await element(by.id('email-input')).replaceText('kingyx3@hotmail.com');
-//     await element(by.id('welcome-text')).multiTap(3);
-//     await element(by.id('submit-email-button')).tap();
+//   await waitFor(element(by.id('activity-header-component'))).toBeVisible().withTimeout(60000);
+//   await expect(element(by.text((host ? 'Hosting' : 'Booking') + ' activity'))).toBeVisible();
+// }
 
-//     // Wait for navigation to complete & perform the visibility checks
-//     await navigateToHomeScreen();
-//   });
+// // Dont need specific testing
+// RootStack
+// HomeTab
+// AuthStack
 
-//   it('Navigate to Account screen', async () => {
-//     await element(by.id('btm-nav-account')).tap();
-
-//     // Wait for navigation to complete & perform the visibility checks
-//     await waitFor(element(by.id('account-header-component'))).toBeVisible().withTimeout(60000);
-//   });
-
-//   it('Change user name', async () => {
-//     const randomText = (Math.floor(1000 + Math.random() * 9000)).toString();
-//     const newUserName = 'newUserName' + randomText + "   "
-//     const oldUserNameLabel = (await element(by.id('username-input')).getAttributes()).label
-
-//     await element(by.id('edit-input')).tap();
-//     await element(by.id('username-input')).tap();
-//     await element(by.id('username-input')).clearText();
-//     await element(by.id('username-input')).typeText(newUserName);
-//     await element(by.id('save-input')).tap(); // Closes keyboard
-//     await element(by.id('save-input')).tap(); // Saves username
-//     // await element(by.id('cancel-input')).tap();
-//     await waitFor(element(by.id('save-input'))).not.toBeVisible().withTimeout(30000); // Saves username
-//     await waitFor(element(by.id('edit-input'))).toBeVisible().withTimeout(30000); // Saves username
-
-//     const newUserNameLabel = (await element(by.id('username-input')).getAttributes()).label
-
-//     console.log("oldUserNameLabel", "|" + oldUserNameLabel + "|")
-//     console.log("newUserNameLabel", "|" + newUserNameLabel + "|")
-//     console.log("newUserName", "|" + newUserName + "|")
-
-//     await expect(element(by.id('username-input'))).toHaveLabel(newUserName);
-//   });
-
-//   it('Change user avatar (needs work)', async () => {
-//     // const randomText = (Math.floor(1000 + Math.random() * 9000)).toString();
-//     // await element(by.id('edit-input')).tap();
-
-
-//     // // Wait for navigation to complete & perform the visibility checks
-//     // await waitFor(element(by.id('username-input'))).toHaveLabel('new_username' + randomText);
-//   });
-
-//   it('Navigate to Activity screen (user)', async () => {
-//     await element(by.id('Your activity_account')).tap();
-
-//     // Wait for navigation to complete & perform the visibility checks
-//     await waitFor(element(by.id('activity-header-component'))).toBeVisible().withTimeout(60000);
-//     await waitFor(element(by.text('Booking activity'))).toBeVisible().withTimeout(60000);
-//   });
-
-//   // it('Attempt user log tap', async () => {
-//   //   await element(by.id('user-top-tab')).tap(); // Not always necessary
-//   //   await waitFor(element(by.id('0_user_log'))).toBeVisible().withTimeout(60000);
-//   //   const userLogLabel = (await element(by.id("0_user_log")).getAttributes()).label
-//   //   console.log(userLogLabel + "USER_CONSOLE_LOG_OUTPUT")
-
-//   //   if (userLogLabel == 'Top up') {
-//   //     // Top up
-//   //     null
-//   //   } else {
-//   //     // Booking (CD)
-//   //     await testBookingDetailFromActivity(false)
-//   //   }
-//   // });
-
-//   it('Navigate to Activity screen (host)', async () => {
-//     await element(by.id('host-top-tab')).tap();
-
-//     // Wait for navigation to complete & perform the visibility checks
-//     await waitFor(element(by.id('activity-header-component'))).toBeVisible().withTimeout(60000);
-//     await waitFor(element(by.text('Hosting activity'))).toBeVisible().withTimeout(60000);
-//   });
-
-//   // it('Attempt host log tap (needs work)', async () => {
-//   //   await element(by.id('host-top-tab')).tap(); // Not always necessary
-//   //   await waitFor(element(by.id('0_host_log'))).toBeVisible().withTimeout(60000);
-//   //   const hostLogLabel = (await element(by.id("0_host_log")).getAttributes()).label
-//   //   console.log(hostLogLabel + "HOST_CONSOLE_LOG_OUTPUT")
-
-//   //   const spaceLogTypeArray = ['Space Created', 'Space Updated', 'Space Disabled', 'Space Enabled', 'Price/Availability Updated']
-
-//   //   if (spaceLogTypeArray.includes(hostLogLabel)) {
-//   //     // Space (CUD)
-//   //     null
-//   //   } else {
-//   //     // Booking (CD)
-//   //     await testBookingDetailFromActivity(true)
-//   //   }
-//   // });
-
-//   it('Navigate back to Account screen', async () => {
-//     await element(by.id("back-button-activity")).tap();
-
-//     await waitFor(element(by.id('account-header-component'))).toBeVisible().withTimeout(60000);
-//   })
-// });
-
-// // const testBookingDetailFromActivity = async (host) => {
-// //   // navigate to BookingDetail screen
-// //   await element(by.id('0_' + (host ? "host" : "user") + '_log')).tap();
-
-// //   await waitFor(element(by.id("booking-detail-scroll-view"))).toBeVisible(100).withTimeout(60000)
-// //   await waitFor(element(by.id("booking-title"))).toBeVisible(100).withTimeout(10000)
-// //   await waitFor(element(by.id("booking-price"))).toBeVisible(100).withTimeout(10000)
-
-// //   await testBookingDetail()
-
-// //   // navigate back to Activity screen
-// //   await element(by.id("back-button-booking-detail")).tap();
-
-// //   await waitFor(element(by.id('activity-header-component'))).toBeVisible().withTimeout(60000);
-// //   await expect(element(by.text((host ? 'Hosting' : 'Booking') + ' activity'))).toBeVisible();
-// // }
-
-// // // Dont need specific testing
-// // RootStack
-// // HomeTab
-// // AuthStack
-
-// // HostStackModal
-// // MessageStackModal
-// // AccountStackModal
+// HostStackModal
+// MessageStackModal
+// AccountStackModal
